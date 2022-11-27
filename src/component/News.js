50762391f26f58c6502c680d073def71539ef8d2
Ankit <sahuankit640@gmail.com>
2022-11-27 18:06:51 +0530

    Here you write message while committing, you can write anything here

--- src/component/News.js
+++ src/component/News.js
@@ -4,65 +4,12 @@ import Spinner from './Spinner';
 import { Outlet } from 'react-router-dom';
 
 export default class News extends Component {
-    articles = [
-                    // {
-                    //     "source": {
-                    //         "id": "bbc-sport",
-                    //         "name": "BBC Sport"
-                    //     },
-                    //     "author": null,
-                    //     "title": "England beat Pakistan to win T20 World Cup",
-                    //     "description": "Watch highlights as Ben Stokes, Sam Curran and Adil Rashid star in helping England claim their second T20 World Cup title with a five-wicket victory over Pakistan in the final.",
-                    //     "url": "http://www.bbc.co.uk/sport/av/cricket/63616028",
-                    //     "urlToImage": "https://ichef.bbci.co.uk/news/1024/cpsprodpb/2FB4/production/_127621221_p0dfxhbg.jpg",
-                    //     "publishedAt": "2022-11-13T15:07:23.2811199Z",
-                    //     "content": "Watch highlights as Ben Stokes, Sam Curran and Adil Rashid star in helping England claim their second T20 World Cup title with a five-wicket victory over Pakistan in the final.\r\nMATCH REPORT: England… [+78 chars]"
-                    // },
-                    // {
-                    //     "source": {
-                    //         "id": "talksport",
-                    //         "name": "TalkSport"
-                    //     },
-                    //     "author": "Phil Spencer",
-                    //     "title": "England win T20 World Cup as they beat Pakistan by five wickets in final thanks to heroics from...",
-                    //     "description": "",
-                    //     "url": "https://talksport.com/sport/cricket/1244844/ben-stokes-england-t20-world-cup-pakistan/",
-                    //     "urlToImage": "https://talksport.com/wp-content/uploads/sites/5/2022/11/ben-stokes-liam-livingstone-england-775095869.jpg?strip=all&quality=100&w=1920&h=1080&crop=1",
-                    //     "publishedAt": "2022-11-13T12:09:07Z",
-                    //     "content": "England have won the T20 World Cup for the second time after beating Pakistan by five wickets in the final in Melbourne.\r\nBen Stokes was the star of the show at the Melbourne Cricket Ground after hit… [+3003 chars]"
-                    // },
-                    // {
-                    //     "source": {
-                    //         "id": "bbc-sport",
-                    //         "name": "BBC Sport"
-                    //     },
-                    //     "author": null,
-                    //     "title": "England beat Pakistan to win T20 World Cup",
-                    //     "description": "England beat Pakistan in a pulsating final in Melbourne to win the Men's T20 World Cup for a second time.",
-                    //     "url": "http://www.bbc.co.uk/sport/cricket/63613694",
-                    //     "urlToImage": "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/16762/production/_127620029_gettyimages-1441111025.jpg",
-                    //     "publishedAt": "2022-11-13T11:52:24.9221302Z",
-                    //     "content": "<table>\r\n<tr><td>Men's T20 World Cup final, Melbourne</td></tr><tr><td>Pakistan 137-8 (20 overs): Masood 38 (28); Curran 3-12, Rashid 2-22</td></tr><tr><td>England 138-5 (19 overs): Stokes 52* (49); … [+1027 chars]"
-                    // },
-                    // {
-                    //     "source": {
-                    //         "id": "news-com-au",
-                    //         "name": "News.com.au"
-                    //     },
-                    //     "author": "Nic Savage",
-                    //     "title": "&#8216;Start the game&#8217;: World Cup final in trouble",
-                    //     "description": "Welcome to news.com.au's live blog of the 2022 Men's T20 World Cup final between England and Pakistan at the MCG.",
-                    //     "url": "https://www.news.com.au/sport/cricket/t20-world-cup-final-england-vs-pakistan-live-coverage/live-coverage/0205c6ca9783c7414b0945b2d2ea3ee6",
-                    //     "urlToImage": "https://content.api.news/v3/images/bin/dc37af8a108961b359f866a08784c8d9",
-                    //     "publishedAt": "2022-11-13T01:12:50Z",
-                    //     "content": "Melbourne woke up to blue skies on Sunday morning, but the gorgeous weather isn't expected to hang around for long.\r\nThe Bureau of Meteorology is currently predicting a very high (near 100 per cent) … [+1170 chars]"
-                    // }
-                ]
+     articles = []
 
     static defaultProps = {
         country : "in",
-        pageSize : 12,
-        category : "general",
+        pageSize : 10,
+        category : "breaking-news",
         categoryName : "General",
         mode : "danger",
         search:""
@@ -72,109 +19,47 @@ export default class News extends Component {
     constructor(){
         super();
         this.state = {
-            // url : this.url,
             articles : this.articles,
             loading : false,
-            page:1,
-            totalResults:0
         }
     }      
     
     async componentWillReceiveProps(nextProps){
-        let url="";
         if(nextProps.search!==""){
-            url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dbddeef57b58421aafd6a26534fd9d8d&page=${this.state.page}&pageSize=100`
-            console.log("search happen")
+            let url=`https://gnews.io/api/v4/search?q="${nextProps.search}"&token=493c64a1276b6cf50012f9756cdd10a8&lang=en&country=${this.props.country}&max=${this.props.pageSize}`;
+            this.setState({loading:true});
+            try{
+                let data = await fetch(url);
+                let parsedData = await data.json();
+                this.setState({
+                    articles : parsedData.articles,
+                    loading:false
+                })
+            }
+            catch{
+                console.log("Result not found");
+            }
         }
         else{
-            url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dbddeef57b58421aafd6a26534fd9d8d&page=${this.state.page}&pageSize=${this.props.pageSize}`
-            console.log("search not")
+            this.componentDidMount();
         }
-        this.setState({loading:true});
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({
-            articles : parsedData.articles,
-            totalResults : parsedData.totalResults,
-            loading:false
-        })
     }
 
     async componentDidMount(){
-        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dbddeef57b58421aafd6a26534fd9d8d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let url=`https://gnews.io/api/v4/top-headlines?token=493c64a1276b6cf50012f9756cdd10a8&topic=${this.props.category}&lang=en&country=${this.props.country}&max=${this.props.pageSize}`;
         this.setState({loading:true});
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({
-            articles : parsedData.articles,
-            totalResults : parsedData.totalResults,
-            loading:false
-        })
+        try{
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            this.setState({
+                articles : parsedData.articles,
+                loading:false
+            })
+        }
+        catch{
+            console.log("api expire");
+        } 
     }
-
-    nextPage = async()=>{
-        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dbddeef57b58421aafd6a26534fd9d8d&page=${this.state.page+1}&pageSize=${this.props.pageSize}`
-        window.scrollTo({
-            top: 0, 
-            behavior: 'auto'
-        });
-        this.setState({loading:true});
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({
-            articles : parsedData.articles,
-            page : this.state.page + 1,
-            loading:false
-        })
-    }
-
-    previousPage = async()=>{
-        let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=dbddeef57b58421aafd6a26534fd9d8d&page=${this.state.page-1}&pageSize=${this.props.pageSize}`
-        window.scrollTo({
-            top: 0, 
-            behavior: 'auto'
-        });
-        this.setState({loading:true});
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({
-            articles : parsedData.articles,
-            page : this.state.page - 1,
-            loading:false
-        })
-    }
-
-    // nextPage = async()=>{
-    //     let url=`https://newsdata.io/api/1/news?apikey=pub_137753f27179f7ab91a9728dd82a2130c71cf&category=${this.props.category}&language=en&country=${this.props.country}&page=${this.state.page+1}`
-    //     window.scrollTo({
-    //         top: 0, 
-    //         behavior: 'auto'
-    //     });
-    //     this.setState({loading:true});
-    //     let data = await fetch(url);
-    //     let parsedData = await data.json();
-    //     this.setState({
-    //         articles : parsedData.articles,
-    //         page : this.state.page + 1,
-    //         loading:false
-    //     })
-    // }
-
-    // previousPage = async()=>{
-    //     let url=`https://newsdata.io/api/1/news?apikey=pub_137753f27179f7ab91a9728dd82a2130c71cf&category=${this.props.category}&language=en&country=${this.props.country}&page=${this.state.page-1}`
-    //     window.scrollTo({
-    //         top: 0, 
-    //         behavior: 'auto'
-    //     });
-    //     this.setState({loading:true});
-    //     let data = await fetch(url);
-    //     let parsedData = await data.json();
-    //     this.setState({
-    //         articles : parsedData.articles,
-    //         page : this.state.page - 1,
-    //         loading:false
-    //     })
-    // }
 
     render() {
         return (
@@ -182,27 +67,26 @@ export default class News extends Component {
                 <h1 style={{color:`${this.props.mode==="danger"?"black":"white"}`}}>{this.props.search==="" ? `ExpressNews - Top ${this.props.categoryName} Headline` : `Showing Result for Search`}</h1>
                 <hr style={{color:`${this.props.mode==="danger"?"black":"white"}`}}></hr>
                 {this.state.loading&&<Spinner/>}
-                <div className="row my-3">
-                    {!this.state.loading&&this.state.articles.map((ele)=>{
-                        if(ele.title.toLowerCase().includes(this.props.search)){
-                        return(
-                            <div className="col-md-4 my-3" key={ele.url}>
-                                <NewsItem title = {ele.title} discription={ele.description} imgUrl={ele.urlToImage?ele.urlToImage:require(".//img/image-not-found-300x169.jpg")} newsUrl={ele.url} author={ele.author} date={ele.publishedAt} source={ele.source.name} mode={this.props.mode}/>
-                            </div>
-                        )
-                        }
-                        else{
-                            return("")
-                        }
-                    })}
-                </div>
-                {!this.state.loading&&this.props.search===""&&<div className="container d-flex justify-content-between">
-                    <button type="button" disabled={this.state.page===1} onClick={this.previousPage} class="btn btn-dark">&larr; Previous</button>
-                    <button type="button" disabled={this.state.page === Math.ceil(this.state.totalResults/this.props.pageSize)} onClick={this.nextPage} class="btn btn-dark">Next &rarr;</button>
-                </div>}
+                {(function(){
+                    if (this.state.articles) {
+                        return <div className="row my-3">
+                                    {!this.state.loading&&this.state.articles.map((ele)=>{
+                                        // if(ele.title.toLowerCase().includes(this.props.search)){
+                                        return(
+                                            <div className="col-md-4 my-3" key={ele.link}>
+                                                <NewsItem title = {ele.title} discription={ele.description} imgUrl={ele.image} newsUrl={ele.url}  date={ele.publishedAt} source={ele.source.name} mode={this.props.mode}/>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                    } else {
+                        return <div className='my-3'>
+                                    <h3>NO Result Found</h3>
+                                </div>
+                    }
+                }).call(this)}
                 <Outlet/>
             </div>
         )
     }
 }
-
